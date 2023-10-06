309a964b6451fcdf8730c05a39f9df9ae109b772
lkendi <lkendi003@gmail.com>
2023-10-06 18:00:26 +0300

    Random number is positive, zero or negative

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,8 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return : Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
@@ -10,5 +13,6 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
+
 	return (0);
 }
