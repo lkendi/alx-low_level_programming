6fa75d2a68dc3319a09f7f0ee22aabb0d1c4975d
lkendi <lkendi003@gmail.com>
2023-10-11 17:52:55 +0300

    Print '_putchar' using write

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,11 @@
 #include <unistd.h>
 /**
  * main - function to print _putchar
- * return: Always 0 (Success)
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	write(1, "_putchar\n", sizeof("_putchar\n"));
-       	return (0);	
+	return (0);
 }
