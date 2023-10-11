17b23a8d57e83a278629973a48228bae3f0362ff
lkendi <lkendi003@gmail.com>
2023-10-12 00:42:19 +0300

    Print multiplication table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * times_table - print the 9 times table starting with 0
+ */
+void times_table(void)
+{
+	int i, j, val;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			val = i * j;
+			
+			if (val > 9)
+			{
+
+				_putchar((val / 10) + '0');
+				_putchar((val % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar(val + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+		}
+	}
+}
