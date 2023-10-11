99016e69d593abbd7beaabf0fdc79b932a199d83
lkendi <lkendi003@gmail.com>
2023-10-12 02:41:55 +0300

    Print n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,39 +5,47 @@
 void print_times_table(int n)
 {
 	int i, j, val;
-	if (n <= 15 && n > 0)
+
+	if (n <= 15 && n >= 0)
 	{	
 		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= 9; j++)
 			{
 				val = i * j;
-				if (val > 9)
-				{
-					_putchar((val / 10) + '0');
-					_putchar((val % 10) + '0');
-			
-					if (j < 9)
-					{
-						{
-                                		        _putchar(',');
-                                        		_putchar(' ');
-                                		}
-					}
-				}
-				else
+				if (j == 0)
 				{
 					_putchar(val + '0');
-					if (j < n)
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-					}
 				}
+				else if (j != 0 && val < 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(val + '0');
+				}
+				else if (val >= 10 && val < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((val / 10) + '0');
+					_putchar((val % 10) + '0');
+				}
+				else if (val >=100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((val / 100) + '0');
+					_putchar(((val / 10) % 10) + '0');
+					_putchar((val % 10) + '0');
+				}
+				
+			}
+				
 		}
 		_putchar('\n');
 		}
 	}
-
 }
