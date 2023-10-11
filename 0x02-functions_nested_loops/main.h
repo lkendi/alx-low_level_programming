b01f9f6b76719bf831125635ad07bdce8c913df9
lkendi <lkendi003@gmail.com>
2023-10-11 18:43:46 +0300

    Header files containing all function prototypes

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,16 @@
-#include <unistd.h>
+#ifndef MAIN_H
+#define MAIN_H
+
 /**
+ * main.h - header file that contains all function protoypes
+ *
  * _putchar - writes the character c to stdout
  * @c: The character to print
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
+ * print_alphabet - print the alphabet in lowercase
  */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+	int _putchar(char c);
+	void print_alphabet(void);
+#endif
