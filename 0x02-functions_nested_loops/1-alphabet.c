c27623037f2aea7154514bce5ef5d69fb5dda618
lkendi <lkendi003@gmail.com>
2023-10-11 18:57:28 +0300

    Print lowercase alphabet using _putchar function

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,11 @@
 #include "main.h"
-/*
- * main - print the alphabet in lowercase
- * Return: Always 0 (Success)
+/**
+ * print_alphabet - prints lowercase alphabet
  */
 void print_alphabet(void)
 {
 	char a;
+
 	for (a = 'a'; a <= 'z'; a++)
 	{
 		_putchar(a);
@@ -14,7 +14,11 @@ void print_alphabet(void)
 	_putchar('\n');
 }
 
-
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	print_alphabet();
