a1333644180761bbf3e40d993a39fc4ed8e20618
lkendi <lkendi003@gmail.com>
2023-10-11 19:42:56 +0300

    Function that checks for lowercase letter

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _islower - check if letter is lowercase
- * @c - ascii value of character to check
+ * @c: ascii value of character to check
  *
  * Return: 1 if c is lowercase, otherwise 0
  */
