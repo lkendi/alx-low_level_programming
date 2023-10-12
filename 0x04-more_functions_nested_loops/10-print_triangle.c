aea289ecc57a8f97798f13455b9c21062734e777
lkendi <lkendi003@gmail.com>
2023-10-12 22:10:28 +0300

    Print triangle of size n

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,9 +9,9 @@ void print_triangle(int size)
 
 	if (size > 0)
 	{
-		for (i = 0; i < n; i++)
+		for (i = 1; i <= size i++)
 		{
-			for (j = n; j > 0; j--)
+			for (j = size; j >= 1; j--)
 			{
 				if (j <= i)
 				{
