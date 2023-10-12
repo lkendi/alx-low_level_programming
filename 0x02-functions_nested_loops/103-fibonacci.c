cd5f9f0ee13685e39100f8079e8a13c76ffaf30d
lkendi <lkendi003@gmail.com>
2023-10-12 21:10:34 +0300

    Sum of even fibonacci terms

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - sum of even values in fibonacci series
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	long n1 = 1;
+	long n2 = 2;
+	long n3;
+	long sum = 0;
+	int i;
+
+	while (i < 4000000)
+	{
+		if (n1 % 2 == 0)
+		{
+			sum += n1;
+		}
+		else
+		{
+			n3 = n1 + n2;
+			n1 = n2;
+			n2 = n3;
+		}
+		i++;
+	}
+	printf("%ld \n", sum);
+	return (0);
+}
+
