a893324db4345db3dd0a7461fb6f6493896d8793
lkendi <lkendi003@gmail.com>
2023-10-12 21:44:49 +0300

    Print diagonal of length n

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_diagonal - print \ character n times to form a diagonal
+ * @n: length of diagonal
+ */
+void print_diagonal(int n)
+{
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			if (i == j)
+			{
+				_putchar('\\');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
