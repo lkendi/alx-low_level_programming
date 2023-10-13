17c3c46742d9f4e967a2dfe78149ab02bfad7649
lkendi <lkendi003@gmail.com>
2023-10-13 20:14:32 +0300

    Fix print_remaining_days() for all dates and all leap years

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -14,8 +14,13 @@ void print_remaining_days(int month, int day, int year)
 {
 	if ((year % 4 == 0 && !(year % 100 == 0)) || year % 400 == 0)
 	{
-		if (month >= 2 && day >= 60)
+		if (month >= 2 && day >= 29)
 		{
+			if (day == 29 && month == 2)
+			{
+				printf("Invalid date: %02d/%02d/%04d\n", month, day - 28, year);
+				return;
+			}
 			day++;
 		}
 
