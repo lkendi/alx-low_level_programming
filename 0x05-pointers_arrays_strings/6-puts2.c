2069c93287246df5f13358ae9f2d43283ee7663e
lkendi <lkendi003@gmail.com>
2023-10-17 04:56:44 +0300

    Print first and every other char of a string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,17 +1,19 @@
 #include "main.h"
 /**
- * puts2 - prints every other character os a string starting with the 1st;
+ * puts2 - prints every other character of a string starting with the 1st;
  * @str: string to print
  */
 
 void puts2(char *str)
 {
-	int i = 0;
-
-	while (str[i] != '\0')
+	while (*str != '\0')
 	{
-		_putchar(str[i]);
-		i += 2;
+		if (*str == '\0')
+		{
+			break;
+		}
+		_putchar(*str);
+		str += 2;
 	}
 	_putchar('\n');
 }
