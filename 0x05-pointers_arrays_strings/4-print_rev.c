ae9e496bb7a3e0ac7cab3580255cb40313353b95
lkendi <lkendi003@gmail.com>
2023-10-17 03:26:54 +0300

    Print string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,5 +6,19 @@
 
 void print_rev(char *s)
 {
+	char *e;
 
+	e = s; /*first, e points to the start of the string*/
+	while (*e != '\0')
+	{
+		e++;
+	}
+	/*now, e points to the end of the string*/
+
+	while (e != s)
+	{
+		e--;
+		_putchar(*e);
+	}
+	_putchar('\n');
 }
