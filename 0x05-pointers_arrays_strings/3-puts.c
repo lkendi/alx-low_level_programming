a8d715dfbba45a34a4edfec57566b5318fcb1c34
lkendi <lkendi003@gmail.com>
2023-10-17 03:11:53 +0300

    Print string using putchar

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -6,13 +6,10 @@
 
 void _puts(char *str)
 {
-	if (str != NULL)
+	while (*str != '\0')
 	{
-		while (*str != '\0')
-		{
-			_putchar(*str);
-			str++;
-		}
-		_putchar('\n');
+		_putchar(*str);
+		str++;
 	}
+	_putchar('\n');
 }
