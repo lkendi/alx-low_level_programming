59881bd349febf092adfbb914f4620161f81a9b2
lkendi <lkendi003@gmail.com>
2023-10-18 23:55:17 +0300

    rot13 encoding

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,17 +1,15 @@
-#include "main.c"
+#include "main.h"
 /**
  * *rot13 - encode a string usingrot13
  * @str: string to encode
  * Return: encoded string
  */
 
-char *rot_13(char *str)
+char *rot13(char *str)
 {
-	char b;
-
 	while (*str != '\0')
 	{
-		if ((*str >= 'a' && *str <= 'z') || (*str >= 'A' && *str <='Z'))
+		if ((*str >= 'a' && *str <= 'z') || (*str >= 'A' && *str <= 'Z'))
 		{
 			*str += 13;
 
@@ -19,7 +17,6 @@ char *rot_13(char *str)
 		}
 
 		str++;
-
 	}
-
+	return (str);
 }
