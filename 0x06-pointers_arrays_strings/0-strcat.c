82b89baf14f81a898dcf876ad67de2df1d6237b4
lkendi <lkendi003@gmail.com>
2023-10-18 19:02:20 +0300

    Concat 2 strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * *_strcat - concat 2 strings
+ * @dest - first string
+ * @src - string to concat to dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *start = dest;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+	
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+
+	return start;
+}
+
+
