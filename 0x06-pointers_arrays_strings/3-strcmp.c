2376f25ee591b635a8f982cd4e0f8a71f2796671
lkendi <lkendi003@gmail.com>
2023-10-18 20:48:21 +0300

    Compare 2 strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,12 +9,15 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	if (*s1 == *s2)
+	while (*s1 != '\0' || *s2 != '\0')
 	{
-		return (0);
-	}
-	else
-	{
-		return (*s1 - *s2);
+		if (*s1 == *s2)
+		{
+			return (0);
+		}
+		else
+		{
+			return (*s1 - *s2);
+		}
 	}
 }
