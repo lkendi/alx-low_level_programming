eedc60058e48bb66254c65c86456e04448513f52
lkendi <lkendi003@gmail.com>
2023-10-19 20:52:12 +0300

    Cpaitalize all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -5,8 +5,34 @@
  * Return: capitalized string
  */
 
-char *cap_string(char *)
+char *cap_string(char *str)
 {
-	char seps = {',',';','.','!','?','"','(',')','{','}'}}
+	int i;
 
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i == 0)
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+				str[i] = str[i] - 32;
+			continue;
+		}
+		if (str[i] == ',' || str[i] == ';' || str[i] == '.'
+				|| str[i] == '!' || str[i] == '?' || str[i] == '\"'
+				|| str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
+		{
+			++i;
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] = str[i] - 32;
+				continue;
+			}
+		}
+		else
+		{
+			if (str[i] >= 'A' && str[i] <= 'Z')
+				str[i] = str[i] + 32;
+		}
+	}
+	return (str);
 }
