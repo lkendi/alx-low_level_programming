a4da8afad16b53d4072d2d7348ed10a9c03d8725
lkendi <lkendi003@gmail.com>
2023-10-25 22:41:28 +0300

    Function prototypes

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,5 +10,6 @@
 	int _sqrt_recursion(int n);
 	int is_prime_number(int n);
 	int is_palindrome(char *s);
+	int palindrome_recursive(char *s, int start, int end);
 	int wildcmp(char *s1, char *s2);
 #endif
