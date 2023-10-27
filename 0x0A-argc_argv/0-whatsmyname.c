7fcd539feb50b69a272b82e1f7d243ed5ec9c8cf
lkendi <lkendi003@gmail.com>
2023-10-27 10:36:06 +0300

    Program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - program that prints its name then newline
+ * @argc: number of command line arguments used to compile the program
+ * @argv: array containing the arguments
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
