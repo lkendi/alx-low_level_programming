6720574434f28c8f673f370fad860e1e2c7e169c
lkendi <lkendi003@gmail.com>
2023-10-31 16:26:53 +0300

    Concat 2 strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,6 +14,15 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i, j, len = 0;
 	char *final_string;
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
 	final_string = malloc(sizeof(s1) + sizeof(s2) + 1);
 	if (final_string == NULL)
 	{
