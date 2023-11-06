33596990d9cdf4e5de2ec036b853cd988d1fb26b
lkendi <lkendi003@gmail.com>
2023-11-06 22:56:15 +0300

    Create a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,39 @@
 #include <stddef.h>
 #include <stdlib.h>
 /**
+ * _strdup - duplicates a string
+ * @s: string to duplicate
+ * Return: pointer to the duplicate of s
+ */
+
+char *_strdup(char *s)
+{
+	int i, len = 0;
+	char *s_copy;
+
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		len++;
+	}
+	s_copy = malloc((len + 1) * sizeof(char));
+	if (s_copy == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		s_copy[i] = s[i];
+	}
+	s_copy += '\0';
+	return (s_copy);
+}
+
+
+/**
  * new_dog - creates a new dog
  * @name: name of the dog
  * @age: age of the dog
@@ -12,15 +45,34 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
+	char *name_copy;
+	char *owner_copy;
 
+	if (name == NULL || owner == NULL)
+	{
+		return (NULL);
+	}
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 	{
 		return (NULL);
 	}
-	new_dog->name = name;
+	name_copy = strdup(name);
+	if (name_copy == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+	owner_copy = strdup(owner);
+	if (owner_copy == NULL)
+	{
+		free(name_copy);
+		free(new_dog);
+		return (NULL);
+	}
+	new_dog->name = name_copy;
 	new_dog->age = age;
-	new_dog->owner = owner;
+	new_dog->owner = owner_copy;
 
 	return (new_dog);
 }
