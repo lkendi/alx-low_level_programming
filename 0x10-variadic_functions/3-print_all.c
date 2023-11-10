b3beb0c5dc237fbeae92a74b009d28e808c50ae7
lkendi <lkendi003@gmail.com>
2023-11-10 05:48:53 +0300

    Variadic functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -10,5 +10,36 @@
 
 void print_all(const char * const format, ...)
 {
+	va_list args;
+	unsigned int i = 0;
+	char *separator = "";
+	const char *current;
 
+	va_start(args, format);
+	while (format[i] && format)
+	{
+		current = &format[i];
+		switch (*current)
+		{
+			case 'c':
+				printf("%s%c", separator, va_arg(args, int));
+				break;
+			case 'f':
+				printf("%s%f", separator, va_arg(args, double));
+				break;
+			case 'i':
+				printf("%s%d", separator, va_arg(args, int));
+				break;
+			case 's':
+				printf("%s%s", separator, va_arg(args, char*));
+				break;
+			default:
+				i++;
+				continue;
+		}
+		separator = ", ";
+		i++;
+	}
+	va_end(args);
+	printf("\n");
 }
