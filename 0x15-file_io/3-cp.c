efc7e888944a3b69c9ac5c889cd5e0175a799547
lkendi <lkendi003@gmail.com>
2023-11-28 05:52:19 +0300

    Added project files

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -24,8 +24,8 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
-	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC);
+	file_to = open(argv[2], O_WRONLY | O_CREAT
+			| O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (file_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
@@ -33,7 +33,7 @@ int main(int argc, char **argv)
 	}
 	while ((read_count = read(file_from, buffer, 1024)) > 0)
 	{
-		write_count = write(file_to, buffer, read);
+		write_count = write(file_to, buffer, read_count);
 		if (write_count != read_count)
 		{
 			close(file_from);
@@ -42,10 +42,10 @@ int main(int argc, char **argv)
 			exit(99);
 		}
 	}
-	if (close(file_from) == -1 || close(file_to) == -1)
-	{
-		put_error(100, "Error: Can't close %d\n", file);
-	}
+	if (close(file_from) == -1)
+		put_error(100, "Error: Can't close %d\n", file_from);
+	if (close(file_to) == -1)
+		put_error(100, "Error: Can't close %d\n", file_to);
 	return (0);
 }
 
@@ -59,6 +59,6 @@ int main(int argc, char **argv)
 
 void put_error(int exit_code, const char *error_message, int fd_value)
 {
-	dprintf(STDERR_FILENO, "%s %d\n", error_message, fd);
+	dprintf(STDERR_FILENO, "%s %d\n", error_message, fd_value);
 	exit(exit_code);
 }
