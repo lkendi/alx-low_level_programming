634d2a3535d0e4cfb728907aac54cb35f40f312b
lkendi <lkendi003@gmail.com>
2024-01-15 08:14:07 +0300

    Add 101-hello_holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,15 @@
 string: db "Hello, Holberton\n", 0
 
 section .text
-global _start
+global start_prog
 
-_start:
+start_prog:
 	mov rax, 1
 	mov rdi, 1
 	mov rsi, string
-	mov rdx, 15
+	mov rdx, 16
 	syscall
+	ret
 
 	mov rax, 60
 	xor rdi, rdi
