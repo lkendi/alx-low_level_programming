0446291fe58e039fa8961b4e9ccc534ea87f46bb
lkendi <lkendi003@gmail.com>
2024-02-03 05:48:10 +0300

    Edit task 5 solution

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -11,10 +11,7 @@ void hash_table_print(const hash_table_t *ht)
 	hash_node_t *current_node;
 
 	if (ht == NULL)
-	{
-		printf("{}");
 		return;
-	}
 
 	printf("{");
 	for (i = 0; i < ht->size; i++)
