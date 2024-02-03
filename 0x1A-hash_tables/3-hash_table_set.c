d21b676152e2e0a74e6d3ac818b917f13b11b49e
lkendi <lkendi003@gmail.com>
2024-02-03 05:09:02 +0300

    Edit task 3 solution

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -36,6 +36,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 				return (0);
 			return (1);
 		}
+		current = current->next;
 	}
 	/*Key not found, create ad new node*/
 	new_node = malloc(sizeof(hash_node_t));
