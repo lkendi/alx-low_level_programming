5918832d41cc22667216858af3142448fb0742ab
lkendi <lkendi003@gmail.com>
2024-02-03 04:38:52 +0300

    Add task 0 solution

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,8 +13,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new_hash_table = malloc(sizeof(hash_table_t));
 	if (new_hash_table == NULL)
 		return (NULL);
+
 	new_hash_table->size = size;
-	new_hash_table->array = malloc(size * sizeof(hash_node_t));
+	new_hash_table->array = malloc(size * sizeof(hash_node_t *));
 	if (new_hash_table->array == NULL)
 	{
 		free(new_hash_table);
