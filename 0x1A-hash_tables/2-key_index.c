ae39fad2a66ce4d56d34c52278962fe5770b6fec
lkendi <lkendi003@gmail.com>
2024-02-03 05:57:51 +0300

    Edit task 2 solution

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -9,11 +9,8 @@
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	hash_table_t *new_hash_table = NULL;
 	unsigned long int hash, idx;
 
-	new_hash_table = malloc(sizeof(hash_table_t));
-	new_hash_table->size = size;
 	hash = hash_djb2(key);
 	idx = hash % size;
 	return (idx);
